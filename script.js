da7990890bd356d8a272ce3550fab50360722662
Carl D'Oleo-Lundgren <carl.doleo.lundgren@gmail.com>
2020-04-29 10:17:18 -0500

    refactored code to use class instead of plain constructors for practice

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ let myLibrary_serialized;
 
 render();
 
-function Book(title, author, pages, readStatus) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.readStatus = readStatus
+class Book {
+    constructor(title, author, pages, readStatus) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.readStatus = readStatus
+    }
 }
 
 function addBookToLibrary() {
